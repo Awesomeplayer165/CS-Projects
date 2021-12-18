05d627d2abd25d2b3dc13672e7784473820fde9c
Awesomeplayer165 <70717139+Awesomeplayer165@users.noreply.github.com>
2021-12-17 23:32:11 -0800

    Starting on Person sorting with C# LINQ

---
+++ Queries/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Queries 
+{
+    class TestClass 
+    {
+
+        static void Main(string[] args)
+        {
+
+            var random = new Random();
+
+            var jacob  = new Person("Jacob Trentini", 14, Sports.soccer);
+            var roneet = new Person("Roneet Dhar",    13, Sports.basketball);
+
+            Console.WriteLine(roneet.Id);
+
+            var people = new List<Person>() {};
+
+            // // Query #1.
+            // IEnumerable<int> filteringQuery =
+            //     from aqi in aqiValues
+            //     where aqi 
+            //     orderby num ascending
+            //     select num;
+
+            // var a = filteringQuery.ToList();
+
+            // foreach (var num in a) { Console.WriteLine(num); }
+
+            // Query #2.
+            string[] groupingQuery = { "carrots", "cabbage", "broccoli", "beans", "barley" };
+            IEnumerable<IGrouping<char, string>> queryFoodGroups =
+                from item in groupingQuery
+                group item by item[0];
+
+            foreach (var food in groupingQuery) { Console.WriteLine(food); }
+        }
+    }
+
+    public enum Sports
+    {
+        basketball,
+        soccer,
+        football,
+        baseball
+    }
+
+    public class Person
+    { 
+        public int    Id            { get; set; }
+        public string Name          { get; set; }
+        public int    Age           { get; set; }
+        public Sports FavoriteSport { get; set; }
+
+        private static int idCounter = 0;
+
+        public Person(string aName, int aAge, Sports aFavoriteSport)
+        {
+            Id            = idCounter;
+            Name          = aName;
+            Age           = aAge;
+            FavoriteSport = aFavoriteSport;
+
+            idCounter ++;
+        }
+
+        public Person(int aAge)
+        {
+            Id            = idCounter;
+            Name          = $"Person {Id}";
+            Age           = aAge;
+            FavoriteSport = Id %2 == 0 ? Sports.basketball : Sports.soccer;
+
+            idCounter ++;
+        }
+    }
+}
